3e7bc36c644ff2dff57e5f7ebaf11898bea8c400
olegan7105 <bubencrutl@mail.ru>
2023-05-11 10:55:43 +0300

    Добавьте файлы проекта.

---
+++ календарь/Core/MyCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace календарь.Core;
+public static class MyCoreClass
+{
+    public static Frame? MyFrameCore { get; set; }
+}
